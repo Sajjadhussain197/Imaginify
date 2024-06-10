e6a4a3f78813f01ccb530f4bfc31b9326c48b524
Sajjadhussain197 <sajjadhussain@9582@gmail.com>
2024-06-10 16:26:43 +0500

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <>
+      <p>
+        
+        Home
+        
+     
+        </p>
+    </>
+  );
+}
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const updateTransfromationPage = () => {
+  return (
+    <div>updateTransfromationPage</div>
+  )
+}
+
+export default updateTransfromationPage
