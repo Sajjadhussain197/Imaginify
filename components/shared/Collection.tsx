1b851dd404a644e6fc8e3ca5293425a3b2e56d6d
Sajjad <sajjadhussain9582@gmail.com>
2024-06-16 23:18:59 +0500

    transformation type is set

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -14,6 +14,7 @@ import {
 import { transformationTypes } from "@/constants";
 import { IImage } from "@/lib/database/models/image.model";
 import { formUrlQuery } from "@/lib/utils";
+import { TransformationTypeKey } from "@/types";
 
 import { Button } from "../ui/button";
 
